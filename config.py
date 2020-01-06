cf18071cf694e29a1d1bd66eb1a9605f77a05082
cole-wilson <wilsoncb@students.osd.wednet.edu>
2020-01-06 14:51:35 +0000

    everything

--- config.py
+++
@@ -1,8 +0,0 @@
-startsplit = 'href="'
-endsplit = '"'
-moz = False
-
-dir_base_path = '/home/runner/l'
-
-html_start = "<div style='height:100vh;margin:0px;text-align:center;background-color:black;color:white;'><div style='height:40vh;'></div>"
-html_end = "</div>"
--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import config,get
+import requests, re
 
-if config.moz:
-  get.rank()
-c = 0
-for x in open('top','r+').read().split('\n'):
-  c = c + 1
-  print('Getting ' + x + ' (' + str(c) + '/' + str(len(open('top','r+').read().split('\n'))-1) + ')')
-  get.main(x)
-  m = open('all','w+')
-  m.write(m.read()+'\n\n===\n\n'+open('results','r').read())
-
-
-
-get.l()
-get.post()
+class Page:
+  def __init__('')
