be0a081bb8151d131dcc58c503eb50c8603fdb35
cole-wilson <wilsoncb@students.osd.wednet.edu>
2020-01-06 14:58:51 +0000

    added Page class

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import requests, re
 
 class Page:
-  def __init__('')
+  def __init__(self, url):
+    try:
+      tmp = str(requests.get(url).content)
+      tmp = tmp.replace('b\'','').replace('\\n','\n')
+    except:
+      print('error.')
+      tmp = ''
+    self.content = tmp
+    self.url = url
+    del tmp
+    
+bob = Page('https://google.com')
+print(bob.content)
+
+#TODO: Add the main loop.
